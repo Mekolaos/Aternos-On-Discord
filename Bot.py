40eb40c474206d5841fa0c4aecc7fd104b420012
Merouane Houmel <merou@Merou.localdomain>
2020-06-05 15:13:50 +0100

    Updating all connection functions to selenium. Fixing eventual bugs

--- Bot.py
+++ Bot.py
@@ -2,9 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from Configure import launch_config
-from lxml import html
-import requests
-from connect_and_launch import start_server
+from connect_and_launch import start_server, get_status
 from dotenv import load_dotenv
 import json
 
@@ -13,45 +11,46 @@ if not os.path.exists(os.path.relpath(".env")):
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-SERVER_STATUS_URI = "https://" + os.getenv("SERVER_STATUS_URI")
+SERVER_STATUS_URI = "http://" + os.getenv("SERVER_STATUS_URI")
+HEADER = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125"}
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('The bot is logged in as {0.user}'.format(client))
+    await client.get_channel(326811093219541002).send("@everyone Hello motherfuckers !")
+
+    
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
+
     if message.content == '--launch server':
         if get_status() == "Offline":
             await message.channel.send("Launching the server boss !")
             await start_server()
-        else:
+
+        if get_status() == "Online":
             await message.channel.send("The server is already Online")
+
+        else :
+            await message.channel.send("An error occured. Either the status server is not responding, or you didn't set the server name correctly.\nTrying to launch server anyway.")
+            await start_server()
+
     if message.content == '--server status':
         status = get_status()
         await message.channel.send("The server is {}".format(status))
+
     if message.content == '--players':
         players = get_number_of_players()
         await message.channel.send("There are {} players on the server".format(players))
+
     if message.content == '--stop server':
         await message.channel.send("Stopping the server not yet implemented.")
-
-def get_status():
-    page = requests.get(SERVER_STATUS_URI)
-    tree = html.fromstring(page.content)
-    status = tree.xpath('/html/body/div/div[3]/div/div/div[1]/span/text()')
-    return status[0]
-def get_number_of_players():
-    page = requests.get(SERVER_STATUS_URI)
-    tree = html.fromstring(page.content)
-    status = tree.xpath('/html/body/div/div[4]/div/div/div/span[1]/text()')
-    return status[0]
-
-    
     
 
 client.run(BOT_TOKEN)
--- connect_and_launch.py
+++ connect_and_launch.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import asyncio
 import time
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-USER = os.getenv('USER')
-PASSWORD = os.getenv('PASSWORD')
+USER = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD_C')
 URL = "https://aternos.org/go/"
+SERVER_STATUS_URI = "http://" + os.getenv("SERVER_STATUS_URI")
+
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -18,18 +23,25 @@ driver = webdriver.Chrome(options=options)
 
 
 async def start_server():
-    driver.get("https://aternos.org/go/")
+    """ Connects to the server by first logging in to the account,
+        then finds the start button, and clicks it.
+        the try except part tries to find the button, and if it doesn't,
+        it continues to loop until the confirm button is clicked."""
+    #TODO : Fix the stupid try except block that doesn't make sense.
+    #TODO : Find a way to contextualize the user instead of reconnecting every time.
+
+    driver.get(URL)
     element = driver.find_element_by_xpath('//*[@id="user"]')
     element.send_keys(USER)
     element = driver.find_element_by_xpath('//*[@id="password"]')
     element.send_keys(PASSWORD)
     element = driver.find_element_by_xpath('//*[@id="login"]')
     element.click()
-    time.sleep(3)
+    asyncio.wait(10)
     
     element = driver.find_element_by_xpath('//*[@id="start"]')
     element.click()
-    time.sleep(3)
+    asyncio.wait(10)
     element = driver.find_element_by_xpath('//*[@id="nope"]/main/div/div/div/main/div/a[1]')
     element.click()
     state = False
@@ -47,5 +59,17 @@ async def start_server():
                 pass
     driver.close()
 
+
+def get_status():
+    # Piece of shit code that returns the fucking status of the server
+    driver.get(SERVER_STATUS_URI)
+    element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/div/div/div[1]/span')))
+    return element.text
+
+def get_number_of_players():
+    # Returns the number of players
+    driver.get(SERVER_STATUS_URI)
+    number_of_players = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[4]/div/div/div/span[1]')))
+    return number_of_players.text
 
 
