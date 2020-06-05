5ec4c1a81e098fcc7decae124c8f3372fc8a9409
Merouane Houmel <merou@Merou.localdomain>
2020-06-06 00:46:34 +0100

    Server connection now not entirely dependent on user coonnection to Aternos

--- connect_and_launch.py
+++ connect_and_launch.py
@@ -23,12 +23,44 @@ driver = webdriver.Chrome(options=options)
 
 
 async def start_server():
-    """ Connects to the server by first logging in to the account,
-        then finds the start button, and clicks it.
-        the try except part tries to find the button, and if it doesn't,
-        it continues to loop until the confirm button is clicked."""
+    """ Starts the server by clicking on the start button.
+        The try except part tries to find the confirmation button, and if it 
+        doesn't, it continues to loop until the confirm button is clicked."""
     #TODO : Fix the stupid try except block that doesn't make sense.
-    #TODO : Find a way to contextualize the user instead of reconnecting every time.
+    
+    element = driver.find_element_by_xpath('//*[@id="start"]')
+    element.click()
+    asyncio.wait(10)
+    element = driver.find_element_by_xpath('//*[@id="nope"]/main/div/div/div/main/div/a[1]')
+    element.click()
+    state = driver.find_element_by_xpath('//*[@id="nope"]/main/section/div[3]/div[2]/div/div/span[2]/span')
+    while state.text == "Waiting in queue":
+        state = driver.find_element_by_xpath('//*[@id="nope"]/main/section/div[3]/div[2]/div/div/span[2]/span')
+        try:
+            element = driver.find_element_by_xpath('//*[@id="confirm"]')
+            element.click()
+        except ElementNotInteractableException as e:
+            pass
+    driver.close()
+
+
+def get_status():
+    # Piece of shit code that returns the fucking status of the server as a string.
+
+    driver.get(SERVER_STATUS_URI)
+    element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/div/div/div[1]/span')))
+    return element.text
+
+def get_number_of_players():
+    # Returns the number of players as a string
+    driver.get(SERVER_STATUS_URI)
+    number_of_players = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[4]/div/div/div/span[1]')))
+    return number_of_players.text
+
+
+async def connect_account():
+    """ Connects to the accounts through a headless chrome tab so we don't
+        have to do it every time we want to start or stop the server."""
 
     driver.get(URL)
     element = driver.find_element_by_xpath('//*[@id="user"]')
@@ -38,38 +70,4 @@ async def start_server():
     element = driver.find_element_by_xpath('//*[@id="login"]')
     element.click()
     asyncio.wait(10)
-    
-    element = driver.find_element_by_xpath('//*[@id="start"]')
-    element.click()
-    asyncio.wait(10)
-    element = driver.find_element_by_xpath('//*[@id="nope"]/main/div/div/div/main/div/a[1]')
-    element.click()
-    state = False
-    while state == False:
-        print("working")
-        status = driver.find_element_by_xpath('//*[@id="nope"]/main/section/div[3]/div[2]/div/div/span[2]/span')
-        if status.text == "Waiting in queue":
-            try:
-                element = driver.find_element_by_xpath('//*[@id="confirm"]')
-                print("found")
-                element.click()
-                state = True
-            except ElementNotInteractableException as e:
-                print(e)
-                pass
-    driver.close()
-
-
-def get_status():
-    # Piece of shit code that returns the fucking status of the server
-    driver.get(SERVER_STATUS_URI)
-    element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/div/div/div[1]/span')))
-    return element.text
-
-def get_number_of_players():
-    # Returns the number of players
-    driver.get(SERVER_STATUS_URI)
-    number_of_players = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[4]/div/div/div/span[1]')))
-    return number_of_players.text
-
 
