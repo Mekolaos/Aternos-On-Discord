d59159cd433b276bd6b47ceb30487fd3a0a530fa
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2022-02-09 11:42:12 +0100

    Added Settings.py
    note: adds easier configuration

---
+++ Settings.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+
+#General required settings
+
+Token = ****
+Username = ****
+Password =  ****
+
+#Discord settings
+description = "Aternos on discord"
+
+
+#Webdriver options
+woptions = webdriver.ChromeOptions()
+woptions.add_argument('--disable-blink-features=AutomationControlled')
+woptions.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                     "AppleWebKit/537.36 (KHTML, like Gecko) "
+                     "Chrome/87.0.4280.88 Safari/537.36")
+woptions.add_experimental_option("excludeSwitches", ["enable-automation"])
+woptions.add_experimental_option('useAutomationExtension', False)
